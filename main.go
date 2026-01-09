d2e63c48874685175fbbd7d5c2df958b4ca54ef3
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-10 00:28:26 +0530

    completed ch-1:l-4

--- main.go
+++ main.go
@@ -2,28 +2,64 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	var chn = make(chan string)
+
+	go func() {
+		var buf = make([]byte, 8)
+		var line string = ""
+
+		for {
+			n, err := f.Read(buf)
+			if err != nil && err != io.EOF {
+				break
+			}
+
+			if n == 0 {
+				break
+			}
+
+			chunk := buf[:n]
+
+			for {
+				sep := bytes.IndexByte(chunk, '\n')
+
+				if sep == -1 {
+					line += string(chunk)
+					break
+				} else {
+					line += string(chunk[:sep])
+					chn <- line
+					line = ""
+					chunk = chunk[sep+1:]
+				}
+			}
+			if err == io.EOF {
+				break
+			}
+		}
+		if line != "" {
+			chn <- line
+		}
+		close(chn)
+	}()
+	return chn
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
-	var buffer = make([]byte, 8)
-	var line string = ""
-
-	for {
-
-		_, err := file.Read(buffer)
-		if err != nil {
-			break
-		}
-
-		n := bytes.Index(buffer, []byte{10}) // 10 -> \n
-		if n != -1 {
-
-		}
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
